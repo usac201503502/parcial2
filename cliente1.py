a71f2402e77c3063c8dd04e4dd916f6a7bc89b7d
usac201503502 <3000808400101@ingenieria.usac.edu.gt>
2020-06-22 13:13:21 -0600

    subida

--- cliente1.py
+++ cliente1.py
@@ -1,17 +1,27 @@
 import paho.mqtt.client as mqtt
+import binascii as bichito
 import logging
 import time
 import os 
 from brokerData import *
 
 logging.basicConfig( #LGHM  configuración inicial del logging
-    level = logging.DEBUG, 
+    level = logging.INFO,
     format = '[%(levelname)s] (%(threadName)-10s) %(message)s'
     )
+
+archivo = open("usuario1.txt", "r")
+com = "comandos"
+com1 ="/03/"+str(archivo.read())
+
 
 
 def on_connect(client, userdata, rc):
     logging.info("Conectado al broker")
+
+def on_publish(client, userdata, mid): 
+    publishText = "Publicacion satisfactoria"
+    logging.debug(publishText)
 
 def on_message(client, userdata, msg):
     #Se muestra en pantalla informacion que ha llegado
@@ -20,25 +30,37 @@ def on_message(client, userdata, msg):
 
 client = mqtt.Client(clean_session=True) #Nueva instancia de cliente
 client.on_connect = on_connect #Se configura la funcion "Handler" cuando suceda la conexion
+client.on_publish = on_publish #Se configura la funcion "Handler" que se activa al publicar algo
 client.on_message = on_message #Se configura la funcion "Handler" que se activa al llegar un mensaje a un topic subscrito
 client.username_pw_set(MQTT_USER, MQTT_PASS) #Credenciales requeridas por el broker
 client.connect(host=MQTT_HOST, port = MQTT_PORT) #Conectar al servidor remoto
 
 #Nos conectaremos a distintos topics:
 qos = 2
+logging.info(com+com1)
 
 #Subscripcion simple con tupla (topic,qos)
 #client.subscribe(("sensores/6/hum", qos))
 
 #Subscripcion multiple con lista de tuplas
-client.subscribe([("comandos/03/201503502", qos), ("comandos/03/201503408", qos), ("comandos/03/201513732", qos)])
+client.subscribe([(str(com+com1), qos), ("comandos/03/201503408", qos), ("comandos/03/201513732", qos)])
 
 
 client.loop_start() #LGHM se inicia el hilo y se mantiene en el fondo esperando publicaciones de suscriptores
 
+comando = b'\x03'
+destino = b'03S01'
+tamaño  = b'44023'
+ 
+suma = comando + b'$' + destino + b'$' + tamaño
+def publishData(topicRoot, topicName, b'value', qos = 2, retain = False):
+    topic = topicRoot + topicName
+    client.publish(topic, value, qos, retain)
+
 try:
     while True:
         logging.info("Esperando publicaciones...")
+        publishData(com, com1,str(suma))
         time.sleep(10)
 
 except KeyboardInterrupt:
