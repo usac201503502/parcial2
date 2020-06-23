45e73e42341bdfeb1ecfb4e7e2981c7874a51a93
usac201503502 <3000808400101@ingenieria.usac.edu.gt>
2020-06-23 13:45:15 -0600

    cliente2

---
+++ cliente2.py
@@ -0,0 +1,67 @@
+import paho.mqtt.client as mqtt
+import binascii as bichito
+import logging
+import threading
+import time
+import os 
+import sys
+from selec import *
+
+logging.basicConfig( #LGHM  configuración inicial del logging
+    level = logging.INFO,
+    format = '[%(levelname)s] (%(threadName)-10s) %(message)s'
+    )
+
+archivo = open("usuario1.txt", "r")
+com = "comandos"
+com1 ="/03/"+str(archivo.read())
+
+def estatus (): #LGHM funcion para hilo de estatus de recepción de datos
+    while True :
+        logging.debug("Esperando publicaciones...")
+        time.sleep(2)
+#def escribir (): #LGHM funcion para hilo pendiente de escritura para envio de datos       
+        #holis = seleccion(input("1) Enviar Texto\n2) Enviar Audio\nSeleccionar: "))
+        #holis.chat()
+        #time.sleep(0.1)
+qos = 2
+client.subscribe([(str(com+com1), qos), ("comandos/03/201503408", qos), ("comandos/03/201513732", qos),("usuarios/03/201503502", qos)])
+
+
+t1 = threading.Thread(name = 'Esperando',
+                        target = estatus,
+                        args = (),
+                        daemon = True
+                        )
+
+#t2 = threading.Thread(name = 'Enviando',
+                        #target = escribir,
+                        #args = (),
+                        #daemon = True
+                        #)                        
+
+client.loop_start() #LGHM se inicia el hilo y se mantiene en el fondo esperando publicaciones de suscriptores
+t1.start()   
+#t2.start()
+
+try:
+    while True:
+        holis = seleccion(input("1) Enviar Texto\n2) Enviar Audio\nSeleccionar: "))
+        holis.chat()
+        time.sleep(1)  
+
+except KeyboardInterrupt:
+    logging.warning("Desconectando del broker...")
+    if t1.isAlive():
+        t1._stop()
+    #if t2.isAlive():
+     #   t2._stop()    
+    
+
+    
+
+finally:
+    client.loop_stop() #Se mata el hilo que verifica los topics en el fondo
+    client.disconnect() #Se desconecta del broker
+    logging.info("Desconectado del broker. Saliendo...")
+    sys.exit()
